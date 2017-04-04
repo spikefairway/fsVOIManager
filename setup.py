0f3322f1ebeda6cf6889d863fe34acb0ee4603e8
spikefairway <k.matsubara91@gmail.com>
2017-04-04 11:44:09 +0900

    Initial import.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'fsVOIManager',
+    version = '0.1.0',
+    author = 'Keisuke Matsubara',
+    author_email = 'k.matsubara91@gmail.com',
+    url = 'https://github.com/spikefairway/fsVOIManager',
+    description = 'fsVOIManager is a tool to merge volume of interests extracted with FreeSurfer',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Unix",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Medical Science Apps."
+        ],
+    )
+
+
+
